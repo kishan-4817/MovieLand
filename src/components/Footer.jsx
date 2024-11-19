277120710adf55a372782e07b3b284e2bf2de163
kishan <kishandhandhukiya8@gmail.com>
2024-11-19 23:35:03 +0530

    redirection check with netlify

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg--800 text-white py-4">
+        <footer className="bg-[#1f2123] shadow-neu text-white py-4">
             <div className="container mx-auto text-center">
                 <p>&copy; 2023 MovieLand. All rights reserved.</p>
             </div>
