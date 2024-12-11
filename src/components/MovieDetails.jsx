d0133578efaf53e0be87c21f33f32f426c40fff7
kishan <kishandhandhukiya8@gmail.com>
2024-12-12 00:50:06 +0530

    update loader

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -61,7 +61,9 @@ const MovieDetails = () => {
                         </div>
                     </div>
                 ) : (
-                    <p>Loading...</p>
+                    <div className="flex justify-center items-center h-full">
+                        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
+                    </div>
                 )}
             </div>
             <Footer />
