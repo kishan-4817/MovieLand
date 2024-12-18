6c95eebfaf756a141595cf9c3ddce86eae42c84c
kishan <kishandhandhukiya8@gmail.com>
2024-12-18 23:00:12 +0530

    header logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,19 +10,23 @@ import {
   Bars3Icon,
   XMarkIcon,
 } from '@heroicons/react/24/outline'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from '../firebase';
+import { Link } from 'react-router-dom'
 
 export default function Header() {
+  const [user] = useAuthState(auth)
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
     <header className="bg-[#1f2123] shadow-neu">
       <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
-          <a href="/" className="-m-1.5 p-1.5">
+          <Link to="/" className="-m-1.5 p-1.5">
             <h3 className="text-4xl tracking-wide font-bold bg-gradient-to-r from-[#f9d3b4] to-transparent bg-clip-text text-transparent">
           MovieLand
           </h3>
-          </a>
+          </Link>
         </div>
         <div className="flex lg:hidden">
           <button
@@ -36,45 +40,51 @@ export default function Header() {
         </div>
         <PopoverGroup className="hidden lg:flex lg:gap-x-12">
           
-          <a href="/" className="text-sm font-semibold leading-6 text-white">
+          <Link to="/" className="text-sm font-semibold leading-6 text-white hover:text-[#f9d3b4]">
             Home
-          </a>
-          <a href="/about" className="text-sm font-semibold leading-6 text-white">
+          </Link>
+          <Link to="/about" className="text-sm font-semibold leading-6 text-white hover:text-[#f9d3b4]">
             About
-          </a>
-          <a href="/contact" className="text-sm font-semibold leading-6 text-white">
+          </Link>
+          <Link to="/contact" className="text-sm font-semibold leading-6 text-white hover:text-[#f9d3b4]">
             Contact
-          </a>
-          <a href="/blog" className="text-sm font-semibold leading-6 text-white">
+          </Link>
+          <Link to="/blog" className="text-sm font-semibold leading-6 text-white hover:text-[#f9d3b4]">
             Blog
-          </a>
+          </Link>
         </PopoverGroup>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end lg:space-x-6">
-          <a href="/Login" className="text-sm font-semibold leading-6 text-white">
-            Log in <span aria-hidden="true">&rarr;</span>
-          </a>
-          <div className="h-6 w-px bg-gray-300/50" aria-hidden="true" />
-          <a href="/Register" className="text-sm font-semibold leading-6 text-white">
-            Register <span aria-hidden="true">&rarr;</span>
-          </a>
+          {user ? (
+            <Link to="/profile" className="text-sm font-semibold leading-6 text-white hover:text-[#f9d3b4]">
+              Profile
+            </Link>
+          ) : (
+            <>
+              <Link to="/Login" className="text-sm font-semibold leading-6 text-white hover:text-[#f9d3b4]">
+                Log in
+              </Link>
+              <div className="h-6 w-px bg-gray-300/50" aria-hidden="true" />
+              <Link to="/Register" className="text-sm font-semibold leading-6 text-white hover:text-[#f9d3b4]">
+                Register
+              </Link>
+            </>
+          )}
         </div>
       </nav>
       <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
         <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-[#1f2123] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="/" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                alt=""
-                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-8 w-auto"
-              />
-            </a>
+            <Link to="/" className="-m-1.5 p-1.5">
+              <span className="sr-only">MovieLand</span>
+              <h3 className="text-4xl tracking-wide font-bold bg-gradient-to-r from-[#f9d3b4] to-transparent bg-clip-text text-transparent">
+              MovieLand
+              </h3>
+            </Link>
             <button
               type="button"
               onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              className="-m-2.5 rounded-md p-2.5 text-white"
             >
               <span className="sr-only">Close menu</span>
               <XMarkIcon aria-hidden="true" className="h-6 w-6" />
@@ -83,32 +93,50 @@ export default function Header() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                <a
-                  href="/about"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                <Link
+                  to="/about"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
                 >
                   About
-                </a>
-                <a
-                  href="/contact"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                </Link>
+                <Link
+                  to="/contact"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
                 >
                   Contact
-                </a>
-                <a
-                  href="/blog"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                </Link>
+                <Link
+                  to="/blog"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
                 >
                   Blog
-                </a>
+                </Link>
               </div>
               <div className="py-6">
-                <a
-                  href="/Login"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </a>
+                {user ? (
+                  <Link
+                    to="/profile"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-50"
+                  >
+                    Profile
+                  </Link>
+                ) : (
+                  <>
+                    <Link
+                      to="/Login"
+                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-50"
+                    >
+                      Log in
+                    </Link>
+                    <div className="h-6 w-px bg-gray-300/50" aria-hidden="true" />
+                    <Link
+                      to="/Register"
+                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-50"
+                    >
+                      Register
+                    </Link>
+                  </>
+                )}
               </div>
             </div>
           </div>
@@ -117,3 +145,4 @@ export default function Header() {
     </header>
   )
 }
+
